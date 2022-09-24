0d0ea62de228761527a8f3cf5c5bf3fca47a77f6
Ralhaddad7 <112815184+Ralhaddad7@users.noreply.github.com>
2022-09-24 09:31:26 +0300

    kk

---
+++ cw77/cw77/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  cw77
+//
+//  Created by Ritaj Alhaddad on 21/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("اختر تخصصك")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ cw77/cw77/cw77App.swift
@@ -0,0 +1,17 @@
+//
+//  cw77App.swift
+//  cw77
+//
+//  Created by Ritaj Alhaddad on 21/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct cw77App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
